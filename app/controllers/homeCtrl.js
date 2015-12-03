157b2d442e86874379cbd9f1c9b4401bd8390ecf
Ivo Spasov <ivospasov89@gmail.com>
2015-12-03 23:08:34 +0200

    info array changed from string to object

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -1,12 +1,22 @@
 'use strict';
 
 portfolio.controller('homeCtrl', ['$scope',
-    function HomeCtrl($scope){
-        var speedHeroInfo = [
-            'Technology - ASP.NET MVC 5',
-            'Languages - C#, HTML5, CSS3',
-            'Database server - MS SQL Server'
-        ];
+    function HomeCtrl($scope) {
+        var speedHeroInfo = [{
+            text: 'Technology - ASP.NET MVC 5'
+        }, {
+            text: 'Languages - C#, HTML5, CSS3'
+        }, {
+            text: 'Database server - MS SQL Server'
+        }, {
+            text: 'ORM - Entity Framework with Code First modeling workflow'
+        }, {
+            text: 'Repository Pattern is used to give abstraction over the data layer'
+        }, {
+            text: 'Inversion of Control Container - ',
+            link: 'http://www.ninject.org/',
+            textForLink: 'Ninject'
+        }];
 
         $scope.speedHeroInfo = speedHeroInfo;
     }
