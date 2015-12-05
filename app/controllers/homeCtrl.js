a1988642d6cad22196b4a27816082705317fba3b
Ivo Spasov <ivospasov89@gmail.com>
2015-12-05 16:40:56 +0200

    Minesweeper added to object

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -9,24 +9,85 @@ portfolio.controller('homeCtrl', ['$scope',
             ' Unregistered users can only see the posts. Registered users can leave comments below the posts.' +
             ' Administrators can create, edit and delete posts and comments.';
 
-        var speedHeroAbout = [
-            'Technology - ASP.NET MVC 5',
-            'Languages - C#, HTML5, CSS3',
-            'Database server - MS SQL Server',
-            'ORM - Entity Framework with Code First modeling workflow',
-            'Repository Pattern is used to give abstraction over the data layer',
-            'Inversion of Control Container - <a href="http://www.ninject.org/">Ninject</a>',
-            '<a href="http://automapper.org/">AutoMapper</a> is used to automatically map the domain model objects to the ViewModel objects',
-            '<a href="http://jquery.com/">jQuery</a> is used to access and manipulate the DOM tree',
-            'Page is responsive on all screen sizes with <a href="http://getbootstrap.com/">Bootstrap</a>',
-            'Controls - Telerik Kendo UI',
-            'AJAX is used for adding new comments',
-            'There is one minute caching on the index page',
-            'There is protection against XSS attack',
-            'IDE - Visual Studio',
-            '<a href="https://git-scm.com/book/en/v2/Getting-Started-About-Version-Control" data-toggle="tooltip" data-placement="top" title="Version control system">VCS - Git</a>',
-            '<a href="https://github.com/ivossss/SpeedHero">Source code</a>'
-        ];
+        var vcsGitLink = '<a href="https://git-scm.com/book/en/v2/Getting-Started-About-Version-Control" class="vcs" title="Version control system">' +
+            'VCS - Git</a>';
+
+        var speedHeroAbout = [{
+            text: 'Technology - ASP.NET MVC 5',
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        }, {
+            text: 'Languages - C#, HTML5, CSS3',
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        }, {
+            text: 'Database server - MS SQL Server',
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        }, {
+            text: 'ORM - Entity Framework with Code First modeling workflow',
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        }, {
+            text: 'Repository Pattern is used to give abstraction over the data layer',
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        }, {
+            text: 'Inversion of Control Container - <a href="http://www.ninject.org/">Ninject</a>',
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        },{
+            text: '<a href="http://automapper.org/">AutoMapper</a> is used to automatically map the domain model objects to the ViewModel objects',
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        },{
+            text: '<a href="http://jquery.com/">jQuery</a> is used to access and manipulate the DOM tree',
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        },{
+            text: 'Page is responsive on all screen sizes with <a href="http://getbootstrap.com/">Bootstrap</a>',
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        },{
+            text: 'Controls - Telerik Kendo UI',
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        },{
+            text: 'AJAX is used for adding new comments',
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        },{
+            text: 'There is one minute caching on the index page',
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        },{
+            text: 'There is protection against XSS attack',
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        },{
+            text: 'IDE - Visual Studio',
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        },{
+            text: vcsGitLink,
+            glyphiconType: 'glyphicon glyphicon-leaf'
+        },{
+            text: '<a href="https://github.com/ivossss/SpeedHero">Source code</a>',
+            glyphiconType: 'glyphicon glyphicon-link'
+        }];
+
+        var minesweeperAbout = [{
+            text: 'Languages - JavaScript, HTML5, CSS3',
+            glyphiconType: 'glyphicon glyphicon-flash'
+        },{
+            text: 'Library - <a href="http://jquery.com/">jQuery</a>',
+            glyphiconType: 'glyphicon glyphicon-flash'
+        },{
+            text: 'Drawing is done with ' +
+            '<a href="https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API">Canvas</a>',
+            glyphiconType: 'glyphicon glyphicon-flash'
+        },{
+            text: 'Page is responsive on all screen sizes with <a href="http://getbootstrap.com/">Bootstrap</a>',
+            glyphiconType: 'glyphicon glyphicon-flash'
+        },{
+            text: 'Recursion algorithm is used to open all empty tiles next to each other',
+            glyphiconType: 'glyphicon glyphicon-flash'
+        },{
+            text: 'IDE - JetBrains WebStorm',
+            glyphiconType: 'glyphicon glyphicon-flash'
+        },{
+            text: vcsGitLink,
+            glyphiconType: 'glyphicon glyphicon-flash'
+        },{
+            text: '<a href="https://github.com/ivossss/Minesweeper">Source code</a>',
+            glyphiconType: 'glyphicon glyphicon-link'
+        }];
 
         var projects = [{
             name: 'Speed Hero',
@@ -34,6 +95,12 @@ portfolio.controller('homeCtrl', ['$scope',
             url: 'http://speedhero.apphb.com/',
             imagePath: 'content/images/SpeedHero.png',
             about: speedHeroAbout
+        },{
+            name: 'Minesweeper',
+            description: 'A JavaScript version of the popular Microsoft Windows game.',
+            url: 'http://ivossss.github.io/Minesweeper/',
+            imagePath: 'content/images/minesweeper.png',
+            about: minesweeperAbout
         }];
 
         $scope.projects = projects;
