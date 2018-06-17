04453bac55b20d3c8b8091ecdd92c00e5fa02ab5
Ivo Spasov <ivospasov89@gmail.com>
2018-06-17 17:07:23 +0300

    Added project image and link.

--- src/app/projects/project-detail/project-detail.component.ts
+++ src/app/projects/project-detail/project-detail.component.ts
@@ -10,15 +10,18 @@ import { ProjectService } from '../project.service';
 })
 export class ProjectDetailComponent implements OnInit {
     project: Project;
+    imagePath: string;
 
     constructor(private projectService: ProjectService, private route: ActivatedRoute) { }
 
     ngOnInit() {
         this.route.params.subscribe(
             (params: Params) => {
-                const projectName = params['projectName'];                
+                const projectName = params['projectName'];
                 this.project = this.projectService.getProject(projectName);
                 // TODO: if project is null then redirect to not found page
+
+                this.imagePath = `/assets/images/${this.projectService.convertToUrl(this.project.name)}.png`;
             }
         );
     }
